ca72fd13091bc254edeab23f159015f3846fe381
Khaled Edlbi <114583392+Khaled-Edlbi@users.noreply.github.com>
2024-02-07 17:19:46 +0300

    Delete sadeem_backend directory

--- sadeem_backend/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ImgurToken, Image, Product, Order
-
-
-admin.site.register(ImgurToken)
-admin.site.register(Image)
-admin.site.register(Product)
-admin.site.register(Order)
--- sadeem_backend/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SadeemBackendConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sadeem_backend'
